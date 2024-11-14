424306b292e7d652d9c29849b5c03b9e59834d94
Waqas Ahmed <waqasahmed.uol@gmail.com>
2024-11-14 14:53:19 +0500

    Initial Commit

---
+++ NavigationRouterExample/NavigationRouterExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationRouterExampleApp.swift
+//  NavigationRouterExample
+//
+//  Created by waqas ahmed on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationRouterExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
